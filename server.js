bdf79b688382a6d32f3966a60b7c9f2ad34e7745
Jeff Ballweg <jeffballweg@gmail.com>
2018-01-23 23:25:45 -0600

    first light. rudimentary scanning from lidar to a browser
    brutally inefficient, but it works

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var url = require('url');
 var fs = require('fs');
 var io = require('socket.io');
 
+const SerialPort = require('serialport')
+const LidarPacket = require('../node_modules/neato-lidar/lib/index.js').LidarPacket
+
+
 var server = http.createServer(function(request, response){
     var path = url.parse(request.url).pathname;
 
@@ -34,12 +38,21 @@ var server = http.createServer(function(request, response){
     }
 });
 
+
+function sendVector(socket, angle, radius){
+  socket.emit('msg', {'content': 'sending vector'});
+  socket.emit('v', {'a': angle, 'r': radius});
+}
+
 server.listen(8001);
 io.listen(server);
 
 var listener = io.listen(server);
 listener.sockets.on('connection', function(socket){
+
   console.log("client connected at: ", new Date());
+
+  socket.emit('msg', {'content': 'hey there'});
 
    //recieve client data
   socket.on('client_data', function(data){
@@ -52,12 +65,50 @@ listener.sockets.on('connection', function(socket){
     // process.stdout.write(data.ctrl);
     control(socket, data);
   });
+
+  var serial;
+  socket.on('conn', function(data){
+    console.log("received request to connect to serial");
+    serial = connectSerial(socket);
+
+    serial.on('data', function (data) {
+      data = new Uint8Array([0xFA].concat(data.slice(0, 21)))
+      socket.emit('msg', {'err': data})
+
+      try {
+        let lp = new LidarPacket(data)
+        lp.measures
+          .filter((m) => !m.invalid)
+          .forEach((m) => sendVector(socket, m.index, m.distance))
+      } catch (e) {
+        console.error(e)
+        //socket.emit('msg', {'err': data})
+      }
+    })
+  });
 });
+
+
+function connectSerial(socket){
+  const serial = new SerialPort('/dev/tty.usbserial-AH06NJYB', {
+    baudrate: 115200,
+    parser: SerialPort.parsers.byteDelimiter([0xfa])
+  });
+
+  socket.emit('msg', {'content': 'serial connected'});
+
+  return serial;
+}
+
+function established(socket){
+  socket.emit('connected to serial');
+}
 
 function control(socket, cmd){
   switch(cmd.direction){
     case 'f':
       console.log("forward "+ cmd.speed);
+      socket.emit('msg', {'content': 'forward ho!'});
       sendVector(socket, 270, 200);
       break;
     case 'b':
@@ -76,8 +127,4 @@ function control(socket, cmd){
       console.log("stop "+ cmd.speed);
       break;
   }
-}
-
-function sendVector(socket, angle, radius){
-  socket.emit('v', {'a': angle, 'r': radius});
 }
